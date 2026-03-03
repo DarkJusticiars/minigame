ba44d416aab559a6066268220bed067f3a671505
darkjusticiars <isamar.garcia@upr.edu>
2026-03-02 21:16:17 -0400

    added obstacle tree and score

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
+
 const backgroundImage = new Image();
 backgroundImage.src = "background.png";
 
-// Create image object
 const playerImage = new Image();
-playerImage.src = "player.png"; // MUST match file name exactly
+playerImage.src = "player.png";
+const obstacleImage = new Image();
+obstacleImage.src = "obstacle.png"
 
 let player = {
     x: 50,
@@ -25,6 +27,9 @@ let obstacle = {
 
 const gravity = 0.5;
 
+let gameSpeed = 5;
+let score = 0;
+
 function gameLoop() {
     update();
     draw();
@@ -32,19 +37,22 @@ function gameLoop() {
 }
 
 function update() {
+
+    score += 1;
+    gameSpeed += 0.001;
+
     // Gravity
     player.velocityY += gravity;
     player.y += player.velocityY;
 
-    // Ground collision
     if (player.y > 130) {
         player.y = 130;
         player.velocityY = 0;
         player.jumping = false;
     }
 
-    // Move obstacle
-    obstacle.x -= 5;
+    // Use gameSpeed instead of fixed 5
+    obstacle.x -= gameSpeed;
 
     if (obstacle.x < -20) {
         obstacle.x = 600;
@@ -57,14 +65,19 @@ function update() {
         player.y < obstacle.y + obstacle.height &&
         player.y + player.height > obstacle.y
     ) {
+        // Reset game instead of just alert
         alert("Game Over");
+
         obstacle.x = 600;
+        score = 0;
+        gameSpeed = 5;
     }
 }
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    // Background
     ctx.drawImage(
         backgroundImage,
         0,
@@ -73,26 +86,30 @@ function draw() {
         canvas.height + 10
     );
 
+    ctx.fillStyle = "white";
+    ctx.font = "20px Arial";
+    ctx.fillText("Score: " + Math.floor(score), 20, 30);
 
-    // Draw the player image
+    // Player image
     ctx.drawImage(
         playerImage,
         player.x,
         player.y,
-        player.width +30,
-        player.height +30
+        player.width + 30,
+        player.height + 30
     );
 
-    // Draw obstacle (still rectangle)
-    ctx.fillRect(
+    // Obstacle
+    ctx.drawImage(
+        obstacleImage,
         obstacle.x,
-        obstacle.y+10,
-        obstacle.width,
-        obstacle.height
+        obstacle.y-15,
+        obstacle.width+50,
+        obstacle.height+50
     );
 }
 
-// Wait until image loads before starting game
+// Start game after player image loads
 playerImage.onload = function () {
     gameLoop();
 };
